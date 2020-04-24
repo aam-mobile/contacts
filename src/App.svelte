805b8e60a675b352d6b5e6122b1f80cf4711080c
AAM <mobile2998@gmail.com>
2020-04-24 08:39:35 -0500

    created new contacts project

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	let name = 'Smith';
+	let age = 20;
+	function AddOne(){
+		//age = age +1;
+		age += 1;
+	}
+</script>
+
+
+<style>
+h1{
+color:purple;
+}
+</style>
+
+
+<h1>Hello {name}, My age is {age} </h1>
+
+<button  class="btn btn-primary" on:click="{AddOne}"  >Change Age</button>
