fef5cb22cc369d2a1fe9f6e951e7382d0dd5ca7a
AAM <mobile2998@gmail.com>
2020-04-24 09:10:49 -0500

    Added input

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
-	let name = 'Smith';
+	let userName = 'Omar';
 	let age = 20;
 	function AddOne(){
 		//age = age +1;
 		age += 1;
+	}
+	function UpdateUserName(event){
+		userName = event.target.value;
 	}
 </script>
 
@@ -15,6 +18,9 @@ color:purple;
 </style>
 
 
-<h1>Hello {name}, My age is {age} </h1>
-
+<h1>Hello {userName}, My age is {age} </h1>
+ <!-- <input type="text" class="form-control" placeholder="Username" value="{userName}" on:input="{UpdateUserName}"  >
+  -->
+  <input type="text" class="form-control" placeholder="Username" bind:value="{userName}"   >
+ 
 <button  class="btn btn-primary" on:click="{AddOne}"  >Change Age</button>
