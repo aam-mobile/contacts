7ddb82d554fd7a20d7fd322498b301af63531ad4
AAM <mobile2998@gmail.com>
2020-05-01 09:45:38 -0500

    added contact card

---
+++ src/components/ContactCard.svelte
@@ -0,0 +1,24 @@
+<script>
+
+    export let userName;
+    export let	jobTitle;
+    export	let	UserImage;
+    export   let description;
+            
+</script>
+
+<style>
+
+</style>
+<div class="row">
+  <div class="col-sm-4">
+        <div class="card" >
+        <img src={UserImage} class="card-img-top" alt={userName}>
+            <div class="card-body">
+            <h5 class="card-title">{userName}</h5>
+            <h6 class="card-title">{jobTitle}</h6>
+            <p class="card-text">{description}</p>
+            </div>
+        </div>
+  </div>
+</div>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,31 +1,89 @@
 <script>
 
-	let userName = 'Omar';
-	let age = 20;
-	function AddOne(){
-		//age = age +1;
-		age += 1;
+	import ContactCard from "./ContactCard.svelte"
+
+	let name = 'Bill Gates';
+	let title = 'Programmer';
+	let image = 'https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X_400x400.jpg';
+	let description = 'Owner of Microsoft';	
+	let formState = 'empty';
+	let contacts = [];
+
+	function addContact()
+	{
+		if(name.trim().length == 0 || title.trim().length == 0  || image.trim().length == 0  || description.trim().length == 0 )
+		{			
+			formState = 'invalid'
+			return;
+		}
+
+		//The input is valid. Go ahead and add the contact.
+
+		contacts = [
+				...contacts,
+				{
+					userName:name,
+					jobTitle:title,
+					imageUrl:image,
+					desc: description
+				}
+		]	
+				
+		formState = 'completed'
 	}
-	function UpdateUserName(event){
-		userName = event.target.value;
-	}
+	
+
 </script>
 <style>
-h1{
-color:purple;
-}
 </style>
  
 <div class="container">
-      <div class="card-deck mb-3 text-center">        
-
-		<h1>Hello {userName}, My age is {age} </h1>
-		<!-- <input type="text" class="form-control" placeholder="Username" value="{userName}" on:input="{UpdateUserName}"  >
-		-->
-		<input type="text" class="form-control" placeholder="Username" bind:value="{userName}"   >
+      
+		<h1>Contacts</h1>
 		
-		<button  class="btn btn-primary" on:click="{AddOne}"  >Change Age</button>
- 
+			<div class="form-group">
+				<label for="txtUserName">User Name</label>
+				<input type="text" class="form-control" id="txtUserName" bind:value={name} >
+			</div>
+			<div class="form-group">
+				<label for="txtJobTitle">Job Title</label>
+				<input type="text" class="form-control" id="txtJobTitle" bind:value={title}  >
+			</div>
+			<div class="form-group">
+				<label for="txtImageUrl">Image URL</label>
+				<input type="text" class="form-control" id="txtImageUrl"  bind:value={image} >
+			</div>
+			<div class="form-group">
+				<label for="txtDescription">Description</label>
+				<input type="text" class="form-control" id="txtDescription"  bind:value={description} >
+			</div>
 
-        </div>
+			<button type="submit" class="btn btn-primary"   on:click ={addContact}   >Add Contact</button>
+		 
+
+		{#if formState == 'invalid'}
+			<div class="alert alert-danger" role="alert">
+				Invalid Input
+				</div>
+		{:else}
+			<p>Please add contact info.</p>
+		{/if}
+
+		{#each contacts as contact, i }
+		<h6>#{i + 1}</h6>
+ 
+<ContactCard
+			userName = {contact.userName}
+			jobTitle ={contact.jobTitle} 
+			UserImage = {contact.imageUrl}
+			description ={contact.desc} 
+			
+			></ContactCard>
+ 
+			
+
+		{/each}
+ 
+ 
+		
 </div>
