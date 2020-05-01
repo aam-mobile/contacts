b6f6c5211806223587e7c293dc7ace5f680e9f44
AAM <mobile2998@gmail.com>
2020-05-01 08:28:39 -0500

    Split app file into components

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,11 @@
 <script>
-
-import AppHeader from './Header.svelte';
-	let userName = 'Omar';
-	let age = 20;
-	function AddOne(){
-		//age = age +1;
-		age += 1;
-	}
-	function UpdateUserName(event){
-		userName = event.target.value;
-	}
+	import AppHeader from './components/Header.svelte';
+	import AppFooter from './components/Footer.svelte';
+	import AppMain from './components/Main.svelte';	
 </script>
-
-
 <style>
-h1{
-color:purple;
-}
 </style>
 
 <AppHeader></AppHeader>
-
-<h1>Hello {userName}, My age is {age} </h1>
- <!-- <input type="text" class="form-control" placeholder="Username" value="{userName}" on:input="{UpdateUserName}"  >
-  -->
-  <input type="text" class="form-control" placeholder="Username" bind:value="{userName}"   >
- 
-<button  class="btn btn-primary" on:click="{AddOne}"  >Change Age</button>
+<AppMain></AppMain>
+<AppFooter></AppFooter>
---
+++ src/components/Main.svelte
@@ -0,0 +1,31 @@
+<script>
+
+	let userName = 'Omar';
+	let age = 20;
+	function AddOne(){
+		//age = age +1;
+		age += 1;
+	}
+	function UpdateUserName(event){
+		userName = event.target.value;
+	}
+</script>
+<style>
+h1{
+color:purple;
+}
+</style>
+ 
+<div class="container">
+      <div class="card-deck mb-3 text-center">        
+
+		<h1>Hello {userName}, My age is {age} </h1>
+		<!-- <input type="text" class="form-control" placeholder="Username" value="{userName}" on:input="{UpdateUserName}"  >
+		-->
+		<input type="text" class="form-control" placeholder="Username" bind:value="{userName}"   >
+		
+		<button  class="btn btn-primary" on:click="{AddOne}"  >Change Age</button>
+ 
+
+        </div>
+</div>
